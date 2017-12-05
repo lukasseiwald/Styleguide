43f29df0be9ec96a9b34ce1423a05c33a6aa96c6
Denise Buder <d.buder@sbg.at>
2017-12-05 11:59:23 +0100

    feature: offcanvasnav done

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,4 @@
 function dropDown() {
     document.getElementById("navDrop").classList.toggle("drop");
     document.getElementById("burgerDrop").classList.toggle("x");
-  //  document.getElementById("header").classList.toggle("h");
 }
