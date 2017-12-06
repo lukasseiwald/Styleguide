abb11be8590ceda710db025bae0d43cc556f50f5
Lukas Seiwald <lseiwald.mmt-b2016@fh-salzburg.ac.at>
2017-12-06 18:56:20 +0100

    navBar updated

--- src/nav.js
+++ src/nav.js
@@ -2,3 +2,16 @@ function dropDown() {
     document.getElementById("navDrop").classList.toggle("drop");
     document.getElementById("burgerDrop").classList.toggle("x");
 }
+
+function KonoNavOn(){
+  console.log("clicking");
+  document.getElementById("HomeButton").classList.toggle("out");
+  document.getElementById("EventButton").classList.toggle("out");
+  document.getElementById("UserButton").classList.toggle("out");
+
+}
+
+
+/*
+
+  */
