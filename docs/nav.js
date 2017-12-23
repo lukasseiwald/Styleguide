16a829dc36c1e8820e47af2e058a833741d341bd
Christoph Mayr <23389130+christoph-clouds@users.noreply.github.com>
2017-12-23 14:56:36 +0100

    added docs folder for gh-pages

---
+++ docs/nav.js
@@ -0,0 +1,27 @@
+function dropDown() {
+    document.getElementById("navDrop").classList.toggle("drop");
+    document.getElementById("burgerDrop").classList.toggle("x");
+}
+
+function KonoNavOn(){
+  document.getElementById("Logo").classList.toggle("out");
+  document.getElementById("HomeButton").classList.toggle("out");
+  document.getElementById("EventButton").classList.toggle("out");
+  document.getElementById("UserButton").classList.toggle("out");
+
+}
+
+function hover(element) {
+    element.setAttribute('src', 'img/LogoKonoRandHover.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'img/LogoKonoRand.png');
+}
+
+
+function ClipBoard(x)
+{
+  let copyText = document.getElementsByClassName(x)[0].innerHTML;
+  copyText.select();
+  document.execCommand("copy");
+}
